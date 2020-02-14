dd740a5248fb8a8f04bc334137b737137d91cced
Emeric Tosi <emeric@3dtrust.fr>
2020-02-14 15:03:17 +0100

    add common function script

--- start.sh
+++ start.sh
@@ -3,9 +3,11 @@
 cd "$(dirname "$0")"
 
 source ~/raspberry-showcase/config.sh
+source ~/raspberry-showcase/scripts/common.sh
 
 while true
 do
+  clear_screen
   bash ~/raspberry-showcase/scripts/$TARGET.sh
   sleep $SLEEP
 done
