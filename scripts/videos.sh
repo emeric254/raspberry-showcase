62cf96d7a55582831231931e2e79d7a585d7ee0a
Émeric Tosi <emeric254@users.noreply.github.com>
2024-06-02 21:39:55 +0200

    wip

--- scripts/videos.sh
+++ scripts/videos.sh
@@ -3,10 +3,10 @@
 source ~/raspberry-showcase/config.sh
 
 # verify omxplayer is installed
-which omxplayer || exit 1
+which mpv || exit 1
 
 FILES=$VIDEO_FOLDER/*
 for f in $FILES
 do
-  omxplayer -b -p -o hdmi --aspect-mode letterbox "$f" > /dev/null 2>&1
+  mpv --realy-quiet "$f" > /dev/null 2>&1
 done
